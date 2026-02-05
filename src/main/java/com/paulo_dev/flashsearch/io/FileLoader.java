2362a7141e48f4faae7439898fc1179e3ee74696
PauloEdSm <pauloeduardotroll9646@gmail.com>
2026-02-05 20:04:28 -0300

    feat(io): initialize load method in FileLoader class

--- src/main/java/com/paulo_dev/flashsearch/io/FileLoader.java
+++ src/main/java/com/paulo_dev/flashsearch/io/FileLoader.java
@@ -1,4 +1,40 @@
 package io;
 
+
 public class FileLoader {
+	String path;
+	String content;
+
+	/**
+	 * Gets the text current.
+	 * @return the current text as a String.
+	 */
+		public FileLoader(String path) {
+		super();
+		this.path = path;
+		this.content = load();
+	}
+	
+	public String getPath() {
+		return path;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	/**
+	 * Gets the text current.
+	 * @return the current text as a String.
+	 */
+	public static String load(){
+	return "";
+		
+	}
+		
+		
 }
